d4e1ab85fcbe6ef672b5899bdd9fa32123d37494
kanghuijin <rkd8527@naver.com>
2023-08-22 18:04:17 +0900

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,33 @@
+import programmers.introduction.No011;
+import programmers.introduction.No012;
+import programmers.introduction.No013;
+import programmers.introduction.No016;
+
+public class Main {
+    public static void main(String[] args) {
+
+//        No011 no011 = new No011();
+//        int[] ints = {89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99};
+//        System.out.println(no011.solution(ints));
+
+
+//        No012 no012 = new No012();
+//
+//        System.out.println(no012.solution(64,6));
+
+
+//        No013 no013 = new No013();
+//
+//        String[] str = {"We", "are", "the", "world!"};
+//
+//        System.out.println(no013.solution(str));
+
+
+        No016 no016 = new No016();
+
+        String str = "nice to meet you";
+
+        System.out.println(no016.solution(str));
+
+    }
+}
---
+++ src/programmers/introduction/No000.java
@@ -0,0 +1,8 @@
+package programmers.introduction;
+//  제목
+/*
+    문제
+* */
+public class No000 {
+    // 정답
+}
