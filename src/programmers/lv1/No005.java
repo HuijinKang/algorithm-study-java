66052c4a47306dca2d4531ceccf30c5b86f5d6c2
kanghuijin <rkd8527@naver.com>
2023-10-18 10:03:34 +0900

    2023/10/18 10:03

--- src/programmers/lv1/No005.java
+++ src/programmers/lv1/No005.java
@@ -2,7 +2,8 @@ package programmers.lv1;
 //  나머지가 1이 되는 수 찾기
 /**
     문제 설명
-    자연수 n이 매개변수로 주어집니다. n을 x로 나눈 나머지가 1이 되도록 하는 가장 작은 자연수 x를 return 하도록 solution 함수를 완성해주세요. 답이 항상 존재함은 증명될 수 있습니다.
+    자연수 n이 매개변수로 주어집니다. n을 x로 나눈 나머지가 1이 되도록 하는 가장 작은 자연수 x를 return 하도록 solution 함수를 완성해주세요.
+    답이 항상 존재함은 증명될 수 있습니다.
 
     제한사항
     3 ≤ n ≤ 1,000,000
@@ -22,6 +23,12 @@ public class No005 {
     public int solution(int n) {
         int answer = 0;
 
+        for (int i = 1; i<= n; i++) {
+            if (n % i == 1) {
+                answer = i;
+                break;
+            }
+        }
         return answer;
     }
 }
